bafce2346381bfe7eb1e87c2e8ec3977bada395d
marianichiporuk <marianichiporuk1@gmail.com>
2023-06-08 20:04:30 +0300

    Completed three tasks of lesson-3.

--- main.js
+++ main.js
@@ -1,55 +1,78 @@
 console.log('Task#1');
 
-let user = 'John Doe';
-console.log(user);
+function palindrome(word) {
+  const reverse = [];
+  let el = 0;
 
-const student = 'Maria';
-console.log(student);
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverse[el] = word[i];
+    el++;
+  }
 
-user = 'Maria';
-console.log(user);
+  let count = 0;
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] !== reverse[i]) {
+      count += count;
+    }
+  }
+
+  return count === 0;
+
+}
+console.log(palindrome('комок'));
 
 console.log('Task#2');
 
-let test = 1;
-test += 1;
-console.log(test);
+const min = function (a, b) {
+  if (a > b) {
+    return b;
+  } else if (a < b) {
+    return a;
+  }
+  console.log('Элементы равны');
+};
 
-test = test + '1';
-console.log(test);
+console.log(min(3, 6));
 
-test -= 1;
-console.log(test);
+const max = function (a, y) {
+  if (a > y) {
+    return a;
+  } else if (a < y) {
+    return y;
+  }
+  console.log('Элементы равны');
+};
 
-console.log(!!test);
+console.log(max(7, 5));
+
+function maxx(a, b) {
+  return a > b ? a : b;
+}
+
+console.log(maxx(1, 6));
+
+function minn(a, b) {
+  return a < b ? a : b;
+}
+
+console.log(minn(1, 0));
 
 console.log('Task#3');
 
-const a = [2, 3, 5, 8];
-let result = 1;
-
-for (let i = 0; i < a.length; i++) {
-  result = result * a[i];
-}
-
-console.log(result);
-
-console.log('Task#4');
-
-const b = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < b.length; i++) {
-  if (b[i] > 5 && b[i] < 10) {
-    console.log(b[i]);
+const zero = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 10 === 0) {
+      const first = arr[i] / 10;
+      if (first === 10) {
+        arr[i] = '1zerozero';
+      } else if (first === 0) {
+        arr[i] = 'zero';
+      } else {
+        arr[i] = first + 'zero';
+      }
+    }
   }
-}
+  return arr;
+};
 
-console.log('Task#5');
-
-const c = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < b.length; i++) {
-  if (c[i] % 2 === 0) {
-    console.log(c[i]);
-  }
-}
+console.log(zero([0, 23, 50, 84, 100, 90, 77, 20, 49, 33]));
